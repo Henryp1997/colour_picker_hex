49925e87c38d2923221f710f87fda2caf8833098
Henry Pickersgill <henry.pickersgill@forefrontrf.com>
2024-12-19 16:03:03 +0000

    add build setup.py and colour square to show actual colour

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QLineEdit, QHBoxLayout, QApplication
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPainter, QColor
 from PyQt5.QtCore import Qt
 from pynput.mouse import Listener, Button
 import pyautogui as pag
@@ -13,7 +13,7 @@ class MainWindow(QWidget):
 
         # Set the window properties
         self.setWindowTitle("Colour Picker")
-        self.setGeometry(100, 100, 400, 75)  # x, y, width, height
+        self.setGeometry(100, 100, 350, 75)  # x, y, width, height
         self.setStyleSheet("background-color: #121212;")
         self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
 
@@ -21,6 +21,7 @@ class MainWindow(QWidget):
         self.button = QPushButton("Start")
         self.button.setStyleSheet("color: white; background-color: #366fc9; border: 2px solid white; border-radius: 5px")
         self.button.setFixedWidth(100)
+        self.button.setFixedHeight(30)
         self.button.clicked.connect(self.execute)
 
         # Colour label
@@ -32,13 +33,21 @@ class MainWindow(QWidget):
         self.textbox = QLineEdit()
         self.textbox.setReadOnly(True)
         self.textbox.setStyleSheet("color: white;")
-        self.textbox.setFixedWidth(200)
-        
+        self.textbox.setFixedWidth(90)
+        self.textbox.setFixedHeight(self.button.height())
+
+        # Square icon to show actual colour
+        self.colour_icon = QLineEdit()
+        self.colour_icon.setReadOnly(True)
+        self.colour_icon.setFixedWidth(self.button.height())
+        self.colour_icon.setFixedHeight(self.button.height())
+
         # Set up layout
         layout = QHBoxLayout()
         layout.addWidget(self.button)
         layout.addWidget(self.label)
         layout.addWidget(self.textbox)
+        layout.addWidget(self.colour_icon)
         self.setLayout(layout)
 
 
@@ -60,6 +69,10 @@ class MainWindow(QWidget):
             self.selecting = False
             self.listener.stop()
 
+            # Reset elements
+            self.textbox.setText("")
+            self.colour_icon.setStyleSheet(f"background-color: #121212")
+
         # Change button style / behaviour
         self.button.setText(text)
         self.button.setStyleSheet(f"color: white; background-color: {colour}; border: 2px solid white; border-radius: 5px")
@@ -70,9 +83,11 @@ class MainWindow(QWidget):
             # screen = ImageGrab.grab()
             # rgb = screen.getpixel((x, y))
             rgb = pag.pixel(x, y)
-            print((x,y), (rgb))
             hex_colour = '#{:02x}{:02x}{:02x}'.format(*rgb)
             self.textbox.setText(hex_colour.upper())
+
+            # Colour in the square beside the textbox to show colour
+            self.colour_icon.setStyleSheet(f"background-color: {hex_colour}")
 
 
 if __name__ == "__main__":
---
+++ setup.py
@@ -0,0 +1,20 @@
+import cx_Freeze
+import os
+
+executables = [
+    cx_Freeze.Executable(
+        f"{os.path.dirname(os.path.realpath(__file__))}/app.py",
+        base="Win32GUI",
+        target_name="colourPicker.exe"
+    )
+]
+
+cx_Freeze.setup(
+    name="pyTerminal",
+    executables=executables,
+    options={
+        "build_exe": {
+            "includes": ["pynput", "pynput.keyboard._win32", "pynput.mouse._win32"]
+        }
+    }
+)
